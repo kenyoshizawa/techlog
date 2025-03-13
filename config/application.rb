52ac9651fb69f0923b00436e5be9fe186b636dd8
kenyoshizawa <harborseal2521@gmail.com>
2025-03-13 17:30:37 +0900

    [Add] config.generatorsの設定追加 #7 アセットやhelperは、generate時に自動生成しない ルーティング定義は、自動生成しない

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Techlog
       g.skip_routes true
       g.template_engine :slim
     end
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
